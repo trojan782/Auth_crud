a50efa4ccaefaa5a82b10ee5c7357c7fef5b3f92
david jacobs <davidjacobs522@gmail.com>
2021-04-24 03:14:50 +0100

    finished sign up but alerts are not showing

--- Process/signProcess.php
+++ Process/signProcess.php
@@ -14,26 +14,26 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     $password = $_POST['password'];
     $cpassword = $_POST['cpassword'];
 
-    // $sql = "Select * from users where username='$username'";
-
-    // $value = mysqli_query($conn, $sql);
-
-    // $numRow = mysqli_num_rows($value);
-
-    // if ($numRow == 0) {
-    if (($password == $cpassword)) {
-        $hashedPass = password_hash($password, PASSWORD_DEFAULT);
-        $result = $mysqli->query("INSERT INTO users (username, email, password) VALUES('$username', '$email', '$hashedPass')") or die($mysqli->error);
-
-        if ($result) {
-            $_SESSION['message'] = "You have successfully been registered!";
-            $_SESSION['msg_type'] = "success";
-            header("location: login.php");
-        } else {
-            $_SESSION['message'] = "There was a problem registering you!";
-            $_SESSION['msg_type'] = "danger";
-            header("location: signup.php");
+    $res = $mysqli->query("SELECT * FROM users WHERE username = '$username'");
+    $row = $res->fetch_array();
+    if(count($row) == 0) {
+        if (($password == $cpassword)) {
+            $hashedPass = password_hash($password, PASSWORD_DEFAULT);
+            $result = $mysqli->query("INSERT INTO users (username, email, password) VALUES('$username', '$email', '$hashedPass')") or die($mysqli->error);
+            if ($result) {
+                $_SESSION['message'] = "You have successfully been registered!";
+                $_SESSION['msg_type'] = "success";
+                header("location: login.php");
+            }if(!$result) {
+                $_SESSION['message'] = "There was a problem registering you!";
+                $_SESSION['msg_type'] = "danger";
+                header("location: signup.php");
+            }
         }
     }
+    if($row['username'] > 0) {
+        $_SESSION['message'] = "Username is unavaliable";
+        $_SESSION['msg_type'] = "warning";
+        header("location: signup.php");
+    }
 }
-   
