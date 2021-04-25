80135ad86c9d0aadfe5ccd7aecddfeb0b31b85b2
david jacobs <davidjacobs522@gmail.com>
2021-04-25 18:41:39 +0100

    changed to prepared statements

--- Process/signProcess.php
+++ Process/signProcess.php
@@ -14,12 +14,18 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     $password = $_POST['password'];
     $cpassword = $_POST['cpassword'];
 
-    $res = $mysqli->query("SELECT * FROM users WHERE username = '$username'");
-    $row = $res->fetch_array();
+    // $stmt = $mysqli->query("SELECT * FROM users WHERE username = '$username'");
+    $query = "SELECT * FROM users WHERE username =  ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param('si', $username);
+    // $row = $res->fetch_array();
     if(count($row) == 0) {
         if (($password == $cpassword)) {
             $hashedPass = password_hash($password, PASSWORD_DEFAULT);
-            $result = $mysqli->query("INSERT INTO users (username, email, password) VALUES('$username', '$email', '$hashedPass')") or die($mysqli->error);
+            // $result = $mysqli->query("INSERT INTO users (username, email, password) VALUES('$username', '$email', '$hashedPass')") or die($mysqli->error);
+            $query = "INSERT INTO users (username, email, password) VALUES(?,?,?)";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param($username, $email, $hashedPass);
             if ($result) {
                 $_SESSION['message'] = "You have successfully been registered!";
                 $_SESSION['msg_type'] = "success";
@@ -37,3 +43,6 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         header("location: signup.php");
     }
 }
+$query = "insert into User (Username,Email,Password,DateOfBirth,Photo,VerificationCode) values (?,?,?,?,?,?)";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("ssssss", $username, $email, $password, $date, $photo, $verification_code);
