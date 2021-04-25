6ae62b48bc0ad1b86c15f83c156b9abcee053f5f
david jacobs <davidjacobs522@gmail.com>
2021-04-26 00:11:27 +0100

    added the sql code for the course db

--- db.sql
+++ db.sql
@@ -4,4 +4,12 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(500) NOT NULL,
   `cpassword` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE `zuri1`.`courses` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `course` VARCHAR(255) NOT NULL,
+  `date` DATE NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
