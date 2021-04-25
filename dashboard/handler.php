38dc7c07c71bf3dba08289e9f762dee1fb2d7aef
david jacobs <davidjacobs522@gmail.com>
2021-04-26 00:20:08 +0100

    created the handler for the crud operations

---
+++ dashboard/handler.php
@@ -0,0 +1,28 @@
+<?php
+// $mysqli = new mysqli('127.0.0.1', 'root', '', 'borrow') or die($mysqli->error);
+
+session_start();
+if (isset($_POST['submit'])) {
+    $name= $_POST['name'];
+    $course = $_POST['course'];
+    $date = $_POST['date'];
+
+    $_SESSION['message'] = "Course has been added!";
+    $_SESSION['msg_type'] = "success";
+
+    header("location: index.php");
+    //query database
+    $mysqli->query("INSERT INTO data (name, course, date) VALUES('$name', '$course', '$date')") or die($mysqli->error);
+}
+
+//to delete from the database
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+
+    $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error);
+
+    $_SESSION['message'] = "Course has been deleted!";
+    $_SESSION['msg_type'] = 'danger';
+
+    header("location: table.php");
+}
