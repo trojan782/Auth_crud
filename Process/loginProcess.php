f1eb015190655c0153ad87a564c612c4a2c7896f
david jacobs <davidjacobs522@gmail.com>
2021-04-26 11:09:33 +0100

    trying to add validation for the login

--- Process/loginProcess.php
+++ Process/loginProcess.php
@@ -7,12 +7,24 @@ if (($_SERVER["REQUEST_METHOD"] == 'POST')) {
     $sql = mysqli_query($conn, "SELECT * FROM users where username='$username' and Password='$password'");
     $row  = mysqli_fetch_array($sql);
     if (is_array($row)) {
-        // $_SESSION["ID"] = $row['ID'];
-        $_SESSION["username"] = $row['username'];
-        $_SESSION["password"] = $row['password'];
+        if( ($_SESSION["username"] = $row['username']) && ( $_SESSION["password"] = $row['password'])) {
+            $_SESSION['message'] ="Welcome" . $_SESSION["username"];
+            header("Location: ./dashboard/index.php");
+        }
+        // $_SESSION["username"] = $row['username'];
+        // $_SESSION["password"] = $row['password'];
         // $_SESSION["Last_Name"]=$row['Last_Name']; 
-        header("Location: ./dashboard/index.php");
-    } else {
-        echo "Invalid credentials";
+        // header("Location: ./dashboard/index.php");
+
+        elseif ($_SESSION["password"] != $row["password"]) {
+            $_SESSION['message'] = "Wrong password password, please try again!";
+            $_SESSION['msg_type'] = 'danger';
+        }
     }
+   
+    // else {
+    //     // echo "Invalid credentials";
+    //     $_SESSION['message'] = "invalid details";
+    //     $_SESSION['msg_type'] = 'danger';
+    // }
 }
