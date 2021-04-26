b9a942264f712afa3ff11f9603882580bca6e4dd
david jacobs <davidjacobs522@gmail.com>
2021-04-26 05:35:50 +0100

    signup now working properly

--- Process/signProcess.php
+++ Process/signProcess.php
@@ -1,50 +1,36 @@
 <?php
-// require_once 'databaseconn.php';
-// $showAlert = false;
-// $showError = false;
-include './Process/databaseconn.php';
 
-session_start();
-$exists = false;
+extract($_POST);
+include("databaseconn.php");
 
-if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-
+if(($_SERVER["REQUEST_METHOD"] == 'POST'))
+{
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $cpassword = $_POST['cpassword'];
 
-    $res = $mysqli->query("SELECT * FROM users WHERE username = '$username'");
-    // $query = "SELECT * FROM users WHERE username =  ?";
-    // $stmt = $mysqli->prepare($query);
-    // $stmt->bind_param('si', $username);
-    $row = $res->fetch_array();
-    if(count($row) == 0) {
+    $sql=mysqli_query($conn,"SELECT * FROM users where username='$username'");
+    if(mysqli_num_rows($sql)>0)
+    {
+        echo "This username is already taken";
+        header("location: signup.php");
+        exit;
+    }
+    if (mysqli_num_rows($sql) == 0) {
         if (($password == $cpassword)) {
-            // $hashedPass = password_hash($password, PASSWORD_BCRYPT);
-            $hashedPass = md5($password);
-            $result = $mysqli->query("INSERT INTO users (username, email, password) VALUES('$username', '$email', '$hashedPass')") or die($mysqli->error);
-            // $query = "INSERT INTO users (username,email,password) VALUES(?,?,?)";
-            // $stmt = $mysqli->prepare($query);
-            // $stmt->bind_param("ssss",$username, $email, $hashedPass);
-            if ($result) {
+            $hashedPass = password_hash($password, PASSWORD_DEFAULT);
+            $query = "INSERT INTO users (username, email, password) VALUES('$username', '$email', '$hashedPass')";
+            $sql = mysqli_query($conn, $query) or die("Could Not Perform the Query");
+            if ($sql) {
                 $_SESSION['message'] = "You have successfully been registered!";
                 $_SESSION['msg_type'] = "success";
                 header("location: login.php");
-            }if(!$result) {
+            } else {
                 $_SESSION['message'] = "There was a problem registering you!";
                 $_SESSION['msg_type'] = "danger";
                 header("location: signup.php");
             }
         }
     }
-    if($row['username'] > 0) {
-        $_SESSION['message'] = "Username is unavaliable";
-        $_SESSION['msg_type'] = "warning";
-        header("location: signup.php");
-    }
 }
-// $query = "insert into User (Username,Email,Password,DateOfBirth,Photo,VerificationCode) values (?,?,?,?,?,?)";
-// $stmt = $conn->prepare($query);
-// $stmt->bind_param("ssssss", $username, $email, $password, $date, $photo, $verification_code);
-
