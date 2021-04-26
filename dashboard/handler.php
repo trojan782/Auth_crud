f8d3ecd810a0c6756fc3a820b64b63dc30beb98f
david jacobs <davidjacobs522@gmail.com>
2021-04-26 23:21:54 +0100

    fixed the delete function in the crud

--- dashboard/handler.php
+++ dashboard/handler.php
@@ -2,6 +2,7 @@
 $mysqli = new mysqli('127.0.0.1', 'root', '', 'zuri1') or die($mysqli->error);
 // require_once "Process/databaseconn.php";
 session_start();
+$update = false;
 if (isset($_POST['submit'])) {
     $name= $_POST['name'];
     $course = $_POST['course'];
@@ -19,7 +20,7 @@ if (isset($_POST['submit'])) {
 if (isset($_GET['delete'])) {
     $id = $_GET['delete'];
 
-    $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error);
+    $mysqli->query("DELETE FROM courses WHERE id=$id") or die($mysqli->error);
 
     $_SESSION['message'] = "Course has been deleted!";
     $_SESSION['msg_type'] = 'danger';
@@ -27,15 +28,26 @@ if (isset($_GET['delete'])) {
     header("location: table.php");
 }
 
-if (isset($_GET['edit'])) {
+if(isset($_GET['edit'])) {
     $id = $_GET['edit'];
     $update = true;
-    // $record = mysqli_query($db, "SELECT * FROM data WHERE id=$id");
-    $record = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error);
+    $result = $mysqli->query("SELECT * FROM courses WHERE id=$id") or die($mysqli->error);
+    if(count($result) == 1) {
+        $row = $result->fetch_array();
+        $name = $row['name'];
+        $course = $row['course'];
+        $date = $_row['date'];
 
-    if (count($record) == 1) {
-        $n = mysqli_fetch_array($record);
-        $course = $n['name'];
-        $address = $n['address'];
     }
+}
+if (isset($_POST['update'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $course = $_POST['course'];
+    $date = $_POST['date'];
+    $mysqli->query("UPDATE courses SET name='$name', course='$course' WHERE id=$id") or die($mysqli->error);
+    $_SESSION['message'] = "Record has been updated!";
+    $_SESSION['msg_type'] = "warning";
+
+    header("location: index.php");
 }
