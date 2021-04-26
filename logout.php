ed5c1fd2081faf4b109faee94fe7a719417c407d
david jacobs <davidjacobs522@gmail.com>
2021-04-26 05:47:07 +0100

    logout is working now

--- logout.php
+++ logout.php
@@ -1,9 +1,13 @@
 <?php
-
-
-    // session_start();
-    // unset($_SESSION["username"]);
-    // header("Location: ../login.php");
-
-  
-    
+// Initialize the session
+session_start();
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
