497f8f0759aee797b839e2e8b75fda38ee3537f5
david jacobs <davidjacobs522@gmail.com>
2021-04-27 22:22:11 +0100

    fixed password reset

--- reset.php
+++ reset.php
@@ -21,7 +21,10 @@
                         <div class='welcome1'>Hello Kindly <b>Reset</b> Your Password</div>
                         <form action="" method="post">
                             <div class='input-fields'>
-                                <input type='password' name="password" placeholder='New Password' class='input-line full-width'></input>
+                                <input type='password' name="cpassword" placeholder='current Password' class='input-line full-width'></input>
+                            </div>
+                            <div class='input-fields'>
+                                <input type='password' name="newPassword" placeholder='New Password' class='input-line full-width'></input>
                             </div>
                             <!-- <div class='spacing'>or <a href="signup.php" class='highlight'>sign up</a></div> -->
                             <div><button class='ghost-round full-width' name="reset">Reset</button></div>
