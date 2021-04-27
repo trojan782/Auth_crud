a64b5eb8d2c346dc8263425dca27fdfa1114269e
david jacobs <davidjacobs522@gmail.com>
2021-04-27 22:22:56 +0100

    fixed the password reset  feature

--- resetProcess.php
+++ resetProcess.php
@@ -1,23 +1,24 @@
 <?php
-include("./Process/databaseconn.php");
-
 session_start();
+$id = $_SESSION["username"];/* username of the user */
+$conn = mysqli_connect('127.0.0.1','root','','zuri1') or die('Unable To connect');
 
-if(($_SERVER["REQUEST_METHOD"] == 'POST')) {
-    $username = $_SESSION['username'];
-    // $Password = $_POST['password'];
-    $sql = mysqli_query($conn, "SELECT * FROM users WHERE username = $username");
-    $row = mysqli_fetch_array($sql); 
-    $newPassword = password_hash($password, PASSWORD_DEFAULT);
-    echo $newPassword;
-    if(is_array($row) && $row > 0) {
-      mysqli_query($conn, "UPDATE users SET password='$newPassword' WHERE username='$username'");
-        if($sql) {
-           header("location: login.php");
-        }
-        else {
-            echo "password not updated!";
-            header("location: reset.php");
-        }
-    }
+if(count($_POST)>0) {
+$result = mysqli_query($conn,"SELECT *from users WHERE username='" . $id . "'");
+$row=mysqli_fetch_array($result);
+$cpassword = $_POST['cpassword'];
+$newPassword = $_POST['newPassword'];
+$hashedPass = password_hash($newPassword, PASSWORD_DEFAULT);
+
+if($cpassword == password_verify($_POST['cpassword'], $row['password'])) {
+   $update =  mysqli_query($conn, "UPDATE users set password= '$hashedPass' WHERE username = '$id'");
+   if($update) {
+       echo "Password changed successfully";
+       header("location: login.php");
+   }
+   else {
+       echo "Unfortunately, the password was not changed";
+   }
+}
+
 }
